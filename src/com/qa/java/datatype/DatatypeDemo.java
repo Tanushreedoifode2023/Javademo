7da9d96e5eb8e7f2ff1cb6cba1e2acf40b4c0096
Tanushreedoifode2023 <tanushree20791@gmail.com>
2023-04-23 11:41:43 +0530

    First commit for java program demo

---
+++ src/com/qa/java/datatype/DatatypeDemo.java
@@ -0,0 +1,28 @@
+package com.qa.java.datatype;
+
+public class DatatypeDemo {
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		DatatypeDemo data=new DatatypeDemo(); 
+		data.openFD();
+		data.openRD();
+		
+	}
+	
+	public void openFD()
+	{
+		System.out.println("open fixed deposit");
+	}
+	
+
+	public void openRD()
+	{
+		System.out.println("open recurring deposit");
+	}
+	
+
+}
